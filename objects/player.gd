30d7382df124b2b01c27a5edaf20a0d0eb6726c9
Onat Saygı <onat.s98@gmail.com>
2023-09-22 06:09:54 +0300

    added rope and rappelling

--- objects/player.gd
+++ objects/player.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -900.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-#var gravity = 0
+
+var rope_on: PhysicsBody2D
+var rope_weight: RigidBody2D
+var on_rope: bool = false
+var rope_joint: Joint2D
 
 const bombScn = preload("res://objects/bomb.tscn")
-var explosives = 6
 var directionPast = 0
 
 func _process(delta):
@@ -19,24 +20,68 @@ func _process(delta):
 		bomb.direction = Vector2(directionPast, -1)
 		get_parent().add_child(bomb)
 	
-	$Sprite2D.flip_h = directionPast != 1
-	#$PointLight2D.scale.x = abs($PointLight2D.scale.x) * int($Sprite2D.flip_h) * 2 - 1
+	$AnimatedSprite2D.flip_h = directionPast != 1
+
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if not on_rope:
+		if not is_on_floor():
+			velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") :#and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Handle Jump.
+		if Input.is_action_just_pressed("ui_accept") :#and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-		directionPast = direction
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+			directionPast = direction
+			$AnimatedSprite2D.play("walk")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			$AnimatedSprite2D.play("stop")
+	
+	if on_rope:
+		global_position = rope_weight.global_position
+		if Input.is_action_just_pressed('ui_up'):
+			$AnimatedSprite2D.play("jumar")
+			var prev_segment = get_node(rope_joint.node_b).prevSegment()
+			rope_weight.global_position = prev_segment.global_position
+			rope_joint.node_b = prev_segment.get_path()
+		elif Input.is_action_just_pressed("ui_down"):
+			on_rope = false
+			velocity = Vector2(0, 0)
+			rope_weight.queue_free()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
 
-	move_and_slide()
+
+func _on_area_2d_body_entered(body):
+	if body is RigidBody2D and not on_rope and Input.is_action_pressed('ui_up'):
+		print(body.name)
+		on_rope = true
+		rope_weight = RigidBody2D.new()
+		add_sibling(rope_weight)
+		rope_weight.collision_layer = 0
+		rope_weight.collision_mask = 0
+		rope_weight.mass = 50
+		rope_weight.global_position = body.global_position
+		
+		rope_joint = PinJoint2D.new()
+		rope_joint.node_a = rope_weight.get_path()
+		rope_joint.node_b = body.get_path()
+		rope_weight.add_child(rope_joint)
+		
+	#print(body)
+	#print(typeof(body))
+	#if body is RigidBody2D and not on_rope and Input.is_action_pressed('ui_up'):
+	#	global_position = body.global_position
+	#	$PinJoint2D.node_b = body.get_path()
+	#	rope_on = body
+	#	on_rope = true
+		#gravity = 0
+		#velocity = position - body.position
+		#move_and_slide()
+	#	velocity = Vector2(0, 0)
--- objects/rope_base.gd
+++ objects/rope_base.gd
@@ -1,63 +1,94 @@
 extends StaticBody2D
 
-const ropeSegment = preload('res://objects/rope_segment.tscn')
-var count = 55
-var lastPath
-var lastPos
-var rope 
-var lastSegment
-var ropePoints: PackedVector2Array = []
-var ropeColors: PackedColorArray = []
-var bezier: Curve2D = Curve2D.new()
+const segment_scene = preload('res://objects/rope_segment.tscn')
+var segment_count: int = 65
+var last_path: NodePath
+var last_pos: Vector2
+var rope: PhysicsBody2D
+var last_segment: PhysicsBody2D
+var rope_points: PackedVector2Array = []
+var rope_colors: Array = []
+
+func _angle_difference(angle_a: float, angle_b: float) -> float:
+	var diff := angle_a - angle_b
+	if abs(diff) > TAU / 2.0:
+		diff -= TAU * sign(diff)
+	return diff
+
+func _bezier_interpolate(line: PackedVector2Array, subdivision: int) -> PackedVector2Array:
+	if subdivision < 1: return line
+	if line.size() < 3: return line
+	var output := PackedVector2Array()
+	for i in range(line.size() - 1):
+		var a: Vector2
+		var b: Vector2
+		var c: Vector2
+		var actual_subdivisions: int
+		a = line[i]
+		b = line[i + 1]
+		var c_index := i + 2
+		if c_index > line.size() - 1:
+			var before_a := line[i - 1]
+			var angle := _angle_difference((b - a).angle(), (a - before_a).angle())
+			c = b + (b - a).rotated(angle)
+			actual_subdivisions = (subdivision) / 2 + 1
+		else:
+			c = line[c_index]
+			actual_subdivisions = subdivision
+		var true_a = lerp(a, b, 0.5) if i != 0 else a
+		var true_c = lerp(b, c, 0.5)
+		for o in range(actual_subdivisions):
+			var t: float = 1.0 / subdivision * o
+			var ab: Vector2 = lerp(true_a, b, t)
+			var bc: Vector2 = lerp(b, true_c, t)
+			output.append(lerp(ab, bc, t))
+	return output
 
 func prevSegment():
 	return self
 
 func _draw():
-	#draw_polyline(ropePoints, Color.AQUA, 5, true)
-	if bezier.point_count > 3:
-		var points = bezier.tessellate()
-		if points:
-			draw_polyline(points, Color.GAINSBORO, 5, true)
-		#draw_polyline_colors(ropePoints, ropeColors, 5, true)
-	pass
+	# Interpolate rope_points to get bezier
+	var bezier_points = _bezier_interpolate(rope_points, 10)
+	if bezier_points:
+		# Set rope colors
+		rope_colors.clear()
+		var palllette = [Color('c9c0b7'), Color('242220'), Color('c9c0b7')]
+		for i in palllette.size():
+			rope_colors.append([])
+			for j in bezier_points.size():
+				rope_colors[i].append(palllette[(i+j)%3])
+		
+		# Draw rope
+		draw_polyline_colors(bezier_points, PackedColorArray(rope_colors[0]), 7, true)
+		draw_polyline_colors(bezier_points, PackedColorArray(rope_colors[1]), 4, true)
+		draw_polyline_colors(bezier_points, PackedColorArray(rope_colors[2]), 1, true)
 
 func _ready():
-	lastPath = self.get_path()
-	lastPos = $Marker2D.position
-	pass
-	for i in count:
-		rope = ropeSegment.instantiate()
+	# Instance x amount of interchained rope segments
+	last_path = self.get_path()
+	last_pos = $Marker2D.position
+	for i in segment_count:
+		rope = segment_scene.instantiate()
 		add_child(rope)
-		rope.position = lastPos
-		rope.get_child(0).node_a = lastPath
+		rope.position = last_pos
+		rope.get_child(0).node_a = last_path
 		
-		lastPath = rope.get_path()
-		lastPos += rope.get_child(1).position
-	lastSegment = rope
+		last_path = rope.get_path()
+		last_pos += rope.get_child(1).position
+	last_segment = rope
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	ropePoints.clear()
-	lastSegment = rope
-	for i in count:
-		ropePoints.append(lastSegment.position)
-		lastSegment = lastSegment.prevSegment()
-		
-	ropeColors.clear()
-	for i in ropePoints.size():
-		var strain = fmod(float(i)/ropePoints.size(), 1)
-		ropeColors.append(Color(1, strain, strain))
-	
-	bezier.clear_points()
-	#for j in range(ropePoints.size()-3, 0, -1)
-	for j in ropePoints.size()-2:
-		#var d = Vector2(0, 10)
-		#var i = ropePoints.size()-j-3
-		var d_in = ropePoints[j+0] - ropePoints[j+1]
-		var d_out = ropePoints[j+2] - ropePoints[j+1]
-		bezier.add_point(ropePoints[j+0], d_in, d_out, 0)
-		#bezier.add_point(ropePoints[j+0], ropePoints[j+1], ropePoints[j+2], 0)
-		#bezier.add_point(ropePoints[i+0], ropePoints[i+1], ropePoints[i+2])
-	bezier.tessellate()
+	# Get rope segment positions
+	rope_points.clear()
+	last_segment = rope
+	for i in segment_count:
+		rope_points.append(last_segment.position)
+		last_segment = last_segment.prevSegment()
+		# simulating rappelling up
+		#if i == 20:
+		#	last_segment.mass = 70 + abs(sin( (Time.get_ticks_msec()%2000)*TAU/2000) )*20
+		#	print(last_segment.mass)
+
+	# Call _draw()
 	queue_redraw()
